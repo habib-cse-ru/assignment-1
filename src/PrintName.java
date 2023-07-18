99f2bdfd1f654da04707f9f3974ac29b229e46a1
Habibur Rahman <hr13338@gmail.com>
2023-07-18 20:10:20 +0600

    printing name is done

---
+++ src/PrintName.java
@@ -0,0 +1,2 @@
+public class PrintName {
+}
---
+++ src/PrintNameRunner.java
@@ -0,0 +1,8 @@
+public class PrintNameRunner {
+    public static void main(String[] args){
+
+        System.out.println("Habibur Rahman");
+        System.out.println("Hasibur Rahman");
+        System.out.println("Hamidur Rahman");
+     }
+}
