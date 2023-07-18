a2718eaa736137f32aaaa97dbf611ebe640a4897
Habibur Rahman <hr13338@gmail.com>
2023-07-18 19:08:20 +0600

    Greeting section is done

---
+++ src/Greeting.java
@@ -0,0 +1,12 @@
+public class Greeting {
+    public void greetHindi(){
+        System.out.println("हेलो");
+    }
+    public void chainise(){
+        System.out.println("你好");
+    }
+    public void bangla(){
+        System.out.println("হ্যালো");
+    }
+
+}
---
+++ src/GreetingRunner.java
@@ -0,0 +1,11 @@
+public class GreetingRunner {
+    public static void main(String[] args) {
+
+        Greeting hindi = new Greeting();
+        Greeting chainise = new Greeting();
+        Greeting bangla = new Greeting();
+        hindi.greetHindi();
+        chainise.chainise();
+        bangla.bangla();
+    }
+}
